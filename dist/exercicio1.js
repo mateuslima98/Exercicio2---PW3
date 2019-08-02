4a8b84332959ecf03265483a3616e8d16c30ba7b
Mateus e Bianca <mateus_lima98@hotmail.com>
2019-08-02 20:36:01 -0300

    Exeercicio1 - TypeScript

---
+++ dist/exercicio1.js
@@ -0,0 +1,11 @@
+var nota1 = 10;
+var nota2 = 5;
+var nota3 = 7;
+var media = (nota1 + nota2 + nota3) / 3;
+console.log("A media é bb " + media);
+if (media >= 7) {
+    console.log("APROVOOU BB");
+}
+else {
+    console.log("REPROVOOUU BB");
+}
---
+++ exercicio1.ts
@@ -0,0 +1,13 @@
+let nota1: number = 10
+let nota2: number = 5
+let nota3: number = 7
+
+let media: number = (nota1 + nota2 + nota3) / 3
+console.log("A media é bb " + media)
+
+if (media >=7){
+    console.log("APROVOOU BB")
+} else {
+    console.log("REPROVOOUU BB");
+    
+}
